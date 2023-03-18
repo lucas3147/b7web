d7a3adea7e3762a68709091521319cc492adaf57
Lucas <lucas.souza3146@gmail.com>
2023-03-18 10:42:47 -0300

    área de log implementada

---
+++ assets/js/classes.js
@@ -0,0 +1,136 @@
+class Character{
+
+    _life = 1;
+    maxLife = 1;
+    attack = 0;
+    defense = 0;
+
+    constructor(name){
+        this.name = name;
+    }
+
+    get life(){
+        return this._life;
+    }
+
+    set life(newLife){
+        this._life = newLife < 0 ? 0 : newLife;
+    }
+}
+
+class Knight extends Character{
+
+    constructor(name){
+        super(name);
+        this.life = 100;
+        this.attack = 10;
+        this.defense = 8;
+        this.maxLife = this.life;
+    }
+}
+
+class Sorcerer extends Character{
+
+    constructor(name){
+        super(name);
+        this.life = 80;
+        this.attack = 15;
+        this.defense = 3;
+        this.maxLife = this.life;
+    }
+}
+
+class LittleMonster extends Character{
+
+    constructor(){
+        super('Little Monster');
+        this.life = 40;
+        this.attack = 4;
+        this.defense = 4;
+        this.maxLife = this.life;
+    }
+}
+
+class BigMonster extends Character{
+
+    constructor(){
+        super('Big Monster');
+        this.life = 120;
+        this.attack = 10;
+        this.defense = 6;
+        this.maxLife = this.life;
+    }
+}
+
+class Stage{
+
+    constructor(fighter1, fighter2, fighter1El, fighter2El, logObj){
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        this.fighter1El = fighter1El;
+        this.fighter2El = fighter2El;
+        this.log = logObj;
+    }
+
+    start() {
+        this.Update();
+
+        this.fighter1El.querySelector('.attack').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
+        this.fighter2El.querySelector('.attack').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
+    }
+
+    Update() {
+        //Fighter 1
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(2)} Hp`;
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+        this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`;
+
+        //Fighter 2
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(2)} Hp`;
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+        this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`;
+    }
+
+    doAttack(attacking, attacked) {
+        if(attacking.life <= 0 || attacked.life <= 0){
+            this.log.addMessage('Atacando cachorro morto.')
+            return;
+        }
+
+        let attackFactor = (Math.random() * 2);
+        let defenseFactor = (Math.random() * 2);
+
+        let actualAttack = attacking.attack * attackFactor;
+        let actualDefense = attacked.defense * defenseFactor;
+
+        if (actualAttack > actualDefense){
+            attacked.life -= actualAttack;
+            this.log.addMessage(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`)
+        }else{
+            this.log.addMessage(`${attacked.name} conseguiu defender...`)
+        }
+
+        this.Update();
+    }
+}
+
+class Log {
+    list = [];
+
+    constructor(listEl){
+        this.listEl = listEl;
+    }
+
+    addMessage(msg){
+        this.list.push(msg);
+        this.render();
+    }
+
+    render(){
+        this.listEl.innerHTML = "";
+
+        for (let i in this.list) {
+            this.listEl.innerHTML += `<li>${this.list[i]}</li>`
+        }
+    }
+}
---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let log = new Log(document.querySelector('.log'));
+let char = new Knight('Lucas');
+let monster = new BigMonster();
+
+const stage = new Stage(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster'),
+    log
+);
+
+stage.start();
+
+
+
