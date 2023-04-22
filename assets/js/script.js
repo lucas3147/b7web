9d23e195e37b7b08cd87bff7e634a78abec7d8d8
Lucas Lima <lucas.souza3146@gmail.com>
2023-04-22 00:13:07 -0300

    projeto inicial

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,10 @@
-let digitalElement = document.querySelector('.digital');
-let sElement = document.querySelector('.p_s');
-let mElement = document.querySelector('.p_m');
-let hElement = document.querySelector('.p_h');
+document.querySelector('.busca').addEventListener('submit', (event) => {
+    event.preventDefault();
 
-function updateClock() {
-    let now = new Date();
-    let hour = now.getHours();
-    let minute = now.getMinutes();
-    let second = now.getSeconds();
+    let input = document.querySelector('#searchInput').value
 
-    digitalElement.innerHTML = `${toFixedHours(hour)}:${toFixedHours(minute)}:${toFixedHours(second)}`;
+    if (input !== '') {
+        console.log(input);
+    }
 
-    let sDeg = (360 / 60) * second - 90;
-    let mDeg = (360 / 60) * minute - 90;
-    let hDeg = (360 / 12) * hour - 90;
-
-    sElement.style.transform = `rotate(${sDeg}deg)`;
-    mElement.style.transform = `rotate(${mDeg}deg)`;
-    hElement.style.transform = `rotate(${hDeg}deg)`;
-}
-
-function toFixedHours(hour) {
-    return ('00' + hour).slice(-2);
-}
-
-setInterval(updateClock, 1000);
-updateClock();
+})
