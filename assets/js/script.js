fcca849714ee612ad6f87ab3a1168ad9cbbff701
Lucas Lima <lucas.souza3146@gmail.com>
2023-04-21 21:11:42 -0300

    projeto Bateria implementado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,43 @@
-let b7validator = {
-    handleSubmit: (event)=>{
-        event.preventDefault();
-        let send = true;
+document.body.addEventListener('keyup', (event) => {
+   playSound(event.code.toLowerCase());
+});
 
-        let inputs = form.querySelectorAll('input');
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value
 
-        b7validator.clearErrors();
+    if (song !== '') {
+        let songArray = song.split('');
 
-        for(let i = 0; i < inputs.length; i++){
-            let input = inputs[i];
-            let check = b7validator.checkInput(input);
+        playComposition(songArray);
+    }
+});
 
-            if (check !== true) {
-                send = false;
-                
-                b7validator.showError(input, check);
-            }
-        }
+function playSound(sound) {
+    let audioElement = document.getElementById(`s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
 
-        if (send) {
-            form.submit();
-        }
-    },
-    checkInput: (input) => {
-        let rules = input.getAttribute('data-rules');
+    if (audioElement) {
+        audioElement.currentTime = 0;
+        audioElement.play();
+    }
 
-        if (rules != null) {
-            rules = rules.split('|');
-            for (let rule in rules) {
-                let rDetails = rules[rule].split('=');
+    if (keyElement) {
+        keyElement.classList.add('active');
 
-                switch(rDetails[0]) {
-                    case 'required':
-                        if (input.value == ''){
-                            return 'Campo não pode ser vazio';
-                        }
-                    break;
-                    case 'min':
-                        if(input.value.length < rDetails[1]){
-                            return `Campo deve ter pelo menos ${rDetails[1]} caracteres`;
-                        }
-                    break;
-                    case 'email':
-                        if (input.value != ''){
-                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                            if (!regex.test(input.value.toLowerCase())) {
-                                return 'Email não reconhecido!';
-                            }
-                        }
-                    break;
-                }
-            }
-        }
-
-        return true;
-    },
-    showError: (input, error) => {
-        input.style.borderColor = 'red';
-
-        let errorElement = document.createElement('div');
-        errorElement.classList.add('error');
-        errorElement.innerHTML = error;
-
-        input.parentElement.insertBefore(errorElement, input.nextElementSibling);
-    } ,
-    clearErrors: () => {
-        let inputs = form.querySelectorAll('input');
-        for (let i = 0; i < inputs.length; i++) {
-            inputs[i].style = '';            
-        }
-
-        let errorElements = document.querySelectorAll('.error');
-        for (let i = 0; i < errorElements.length; i++) {
-            errorElements[i].remove();
-        }
+        setTimeout(() => {
+            keyElement.classList.remove('active');
+        }, 300);
     }   
-};
+}
 
-let form = document.querySelector('.b7validator');
-form.addEventListener('submit', b7validator.handleSubmit);
+function playComposition(songArray) {
+    let wait = 0;
 
+    for(let songItem of songArray) {
+        setTimeout(() => {
+            playSound(`key${songItem}`);
+        }, wait);
+
+        wait += 250;
+    }
+}
