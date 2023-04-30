dbfa1bca990c39e7d34b348ad414cde55df2bb37
Lucas Lima <lucas.souza3146@gmail.com>
2023-04-30 15:59:39 -0300

    projetoArrastaSolta

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+let areas = {
+    a: null,
+    b: null,
+    c: null
+}
+
 document.querySelectorAll('.item').forEach(item => {
     item.addEventListener('dragstart', dragStart);
     item.addEventListener('dragend', dragEnd);
@@ -9,6 +15,10 @@ document.querySelectorAll('.area').forEach(area => {
     area.addEventListener('drop', drop);
 })
 
+document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral);
+document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral);
+document.querySelector('.neutralArea').addEventListener('drop', dropNeutral);
+
 // Functions Item
 function dragStart(e) {
     e.currentTarget.classList.add('dragging');
@@ -19,14 +29,59 @@ function dragEnd(e) {
 }
 
 // Functions Area
-function dragOver() {
-    console.log('Passou por cima!')
+function dragOver(e) {
+    if (e.currentTarget.querySelector('.item') === null) {
+        e.preventDefault();
+        e.currentTarget.classList.add('hover');
+    }
 }
 
-function dragLeave() {
-    console.log('Saiu!')
+function dragLeave(e) {
+    e.currentTarget.classList.remove('hover');
 }
 
-function drop() {
-    console.log('Soltou!')
+function drop(e) {
+    e.currentTarget.classList.remove('hover');
+    
+    if (e.currentTarget.querySelector('.item') === null) {
+        let dragItem = document.querySelector('.item.dragging');
+        e.currentTarget.appendChild( dragItem );
+        updateAreas();
+    }
+}
+
+// Functions Neutral Area
+function dragOverNeutral(e) {
+    e.preventDefault();
+    e.currentTarget.classList.add('hover');
+}
+
+function dragLeaveNeutral(e) {
+    e.currentTarget.classList.remove('hover');
+}
+
+function dropNeutral(e) {
+    e.currentTarget.classList.remove('hover');
+    let dragItem = document.querySelector('.item.dragging');
+    e.currentTarget.appendChild( dragItem );
+    updateAreas();
+}
+
+// Logic Functions
+function updateAreas() {
+    document.querySelectorAll('.area').forEach(area => {
+        let name = area.getAttribute('data-name');
+
+        if (area.querySelector('.item') !== null) {
+            areas[name] = area.querySelector('.item').innerHTML;
+        } else {
+            areas[name] = null;
+        }
+    });
+
+    if(areas.a === '1' && areas.b === '2' && areas.c === '3') {
+        document.querySelector('.areas').classList.add('correct');
+    }else{
+        document.querySelector('.areas').classList.remove('correct');
+    }
 }
