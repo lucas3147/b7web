5fc3efe71c0466396b1f060c0029c5dfc390bf3a
Lucas Lima <lucas.souza3146@gmail.com>
2023-05-01 10:58:17 -0300

    pesquisa e voto nulo implementado

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,12 @@ function comecarEtapa() {
     let numeroHTML = '';
 
     for (let i = 0; i < etapa.numeros; i++){
-        numeroHTML += '<div class="numero"></div>';
+        if (i === 0) {
+            numeroHTML += '<div class="numero pisca"></div>';
+        }else {
+            numeroHTML += '<div class="numero"></div>';
+        }
+        
     }
 
     seuVotoPara.style.display = 'none';
@@ -26,8 +31,48 @@ function comecarEtapa() {
 
 }
 
+function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter(item => {
+        if (item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    if(candidato.length > 0) {
+        candidatoAtual = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidatoAtual.nome} <br/> Partido: ${candidatoAtual.partido}`
+        
+        let fotosHtml = '';
+        for (let i in candidatoAtual.fotos) {
+            fotosHtml += `<div class="d-1-image"><img src="assets/images/${candidatoAtual.fotos[i].url}" alt="foto do candidato" />${candidatoAtual.fotos[i].legenda}</div>`
+        }
+
+        lateral.innerHTML = fotosHtml;
+    } else {
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
+    }
+}
+
 function clicou(n) {
-    alert("Clicou em " + n);
+    let elNumero = document.querySelector('.numero.pisca');
+    if (elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+            atualizaInterface();
+        }
+    }
 }
 
 function branco() {
